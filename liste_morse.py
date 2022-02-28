6380f4541bd4a0d65c2b3e4815dfb44366d622c5
Delcommune <delcommune.fobagra@gmail.com>
2022-02-28 11:51:37 +0100

    Finalisation du programme morse

--- liste_morse.py
+++ liste_morse.py
@@ -1,4 +1,4 @@
-alphabet=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+alphabet=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"," "]
 morse=[".-",
 "-...",
 "-.-.",
@@ -24,8 +24,24 @@ morse=[".-",
 ".--",
 "-..-",
 "-.--",
-"--.."]
+"--..",
+" "]
 
 #print(alphabet.__len__())
 #print(morse.__len__())
 #pour vérifier le nombre d'elements dans chaque liste, length
+
+#encode lettre > code morse
+#on cherche l'index de la lettre dans la liste alphabet
+def encode(lettre):
+    index=alphabet.index(lettre)
+    #on recherche le code morse correspondant a l'index
+    code_morse=morse[index]
+    #on renvoit le code morse trouve
+    return code_morse
+
+#decode code morse > lettre
+def decode(code):
+    index=morse.index(code)
+    lettre=alphabet[index]
+    return lettre
--- programme_morse.py
+++ programme_morse.py
@@ -6,16 +6,46 @@
 #afficher le code morse correspondant a l'index trouve
 
 
+from tkinter import Variable
 import liste_morse
 #pour importer la librairie
 #! toujours importer les librairies en debut de code
 
-print("Tapez une lettre")
-lettre=input("--->")
+#print("Tapez une lettre")
+#lettre=input("--->")
+
+print("Tapez un mot")
+mot=input("--->")
+code=""
+#pour poser qu'on va entrer une chaine de caracteres
+
+#pour chaque lettre dans le mot, on encode la lettre en morse
+#une fois le mot encode, on l'affiche
+for lettre in mot:
+    code=code+liste_morse.encode(lettre)
+    code=code+" "
+#pour mettre des espaces entre chaque lettre
 
 
-
-index=liste_morse.alphabet.index(lettre)
-print(liste_morse.morse[index])
+#on appelle notre fonction encode dans notre librairie et on stocke le code recupere
+#code=liste_morse.encode(lettre)
+print(code)
 
 #pour lui dire ou trouver les elements, il faut donner le nom de la librairie. avant (ici, "liste_morse.")
+
+print("entrez un code morse")
+code=input("--->")
+mot=""
+
+
+liste_code=str.split(code," ")
+print(liste_code)
+
+#pour chaque element de la liste_code,
+#on recupere la lettre correspondante
+#et on la stocke dans la variable mot
+
+for element in liste_code:
+    mot=mot+liste_morse.decode(element)
+
+print(mot)
